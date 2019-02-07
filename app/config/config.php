86e156d803882054afd9afd38bd6bfd070d2f1ed
sk <sk@sk-ubuntu>
2019-02-08 01:36:32 +0200

    update

--- app/config/config.php
+++ app/config/config.php
@@ -28,8 +28,8 @@
 
 // SMTP
     define('SMTP_HOST', 'smtp.mail.ru'); // Укажите основной и резервный SMTP-серверы
-    define('SMTP_USER', 'kuseal64@mail.ru'); // SMTP username
-    define('SMTP_PASS', 'Fvthbrfytw1964'); // SMTP password
+    define('SMTP_USER', 'test@example.com'); // SMTP username
+    define('SMTP_PASS', 'xxx'); // SMTP password
     define('SMTP_PORT', 587);
 
     define('NUM_PASS', 7); // Колличество символов для восстановления пароля
